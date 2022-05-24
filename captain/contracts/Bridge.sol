8419558b3e7465cdc24653365e84c4cf4264d28e
Satyam Kulkarni <punekar.satyam@gmail.com>
2022-05-24 10:38:21 +0530

    added single interface for all the bridges + updated hyphen provider

--- captain/contracts/Bridge.sol
+++ captain/contracts/Bridge.sol
@@ -1,23 +1,70 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.13;
 
+interface IDex {
+    function transfer(DexData calldata dex) external;
+}
+
+interface IBridge {
+    function transferNative(uint amount, address receiver, uint256 toChainId, string calldata tag) external payable;
+    function transferERC20(uint256 toChainId,
+        address tokenAddress,
+        address receiver,
+        uint256 amount,
+        string calldata tag ) external;
+
+}
+
 contract WagpayBridge {
-    uint256 immutable chainId = 80001;
-
+	address private constant NATIVE_TOKEN_ADDRESS = address(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE);
     
-
-    modifier checkSourceAndToChainId() {
-
-        _;
+    struct DexData {
+        address dex;
+        uint amountToGet;
+        uint fees;
+        uint chainId;
+        address fromToken;
+        address toToken;
     }
 
-    constructor() {}
-
-    function transferNative(uint256 value, address _to, uint256 toChainId) public {
-        
+    struct RouteData {
+        address receiver;
+        address bridge;
+        uint toChain;
+        address fromToken;
+        uint amount;
+        bool dexRequired;
+        DexData dex;
     }
 
-    function transferERC20(uint256 value, address _to, uint256 toChainId, address tokenAddress) public {
+    function transfer(RouteData calldata _route) external payable {
+        if(_route.dexRequired) {
+            IDex dex = IDex(_route.dex.dex);
+            IBridge bridge = IBridge(_route.bridge);
+            
+            // Dex
+            if(_route.dex.fromToken == NATIVE_TOKEN_ADDRESS) {
+                dex.transfer{value: _route.amount}(_route.dex);
+            } else {
+                IERC20(_route.dex.fromToken).approve(_route.dex.dex, _route.amount);
+                dex.transferERC20(_route.dex);
+            }
 
+            // Bridge
+            if(_route.fromToken == NATIVE_TOKEN_ADDRESS) {
+                bridge.transferNative{value: _route.dex.amountToGet}(_route.dex.amountToGet, _route.receiver, _route.toChain, "WagPay");
+            } else {
+                IERC20(_route.fromToken).approve(_route.bridge, _route.dex.amountToGet);
+                bridge.transferERC20(_route.toChain, _route.fromToken, _route.receiver, _route.dex.amountToGet, "WagPay");
+            }
+        } else {
+            // Bridge
+            if(_route.fromToken == NATIVE_TOKEN_ADDRESS) {
+                bridge.transferNative{value: _route.amount}(_route.amount, _route.receiver, _route.toChain, "WagPay");
+            } else {
+                IERC20(_route.fromToken).approve(_route.bridge, _route.amount);
+                bridge.transferERC20(_route.toChain, _route.fromToken, _route.receiver, _route.amount, "WagPay");
+            }
+        }
     }
 }
--- captain/contracts/bridges/HyphenProvider.sol
+++ captain/contracts/bridges/HyphenProvider.sol
@@ -3,40 +3,44 @@ pragma solidity 0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "../interface/ILiquidityPoolHyphen.sol";
-import "../BaseImpl.sol";
-import "hardhat/console.sol";
 
-contract HyphenProvider is BaseImpl, Ownable, ReentrancyGuard {
+contract HyphenProvider is ReentrancyGuard {
 	using SafeERC20 for IERC20;
-
-	address private constant NATIVE_TOKEN_ADDRESS = address(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE);
 	
 	// Hyphen Liquidty Pool Address
 	ILiquidityPool hyphenLiquidityPool;
-	
-	error AmountNotZero();
+
+	event NativeFundsTransferred(address receiver, uint256 toChainId);
+	event ERC20FundsTransferred(address receiver, uint256 toChainId, uint256 value, address tokenAddress);
 
 	constructor(address _hyphen) {
 		hyphenLiquidityPool = ILiquidityPool(_hyphen);
 	}
 
-	function transferNative(address _from, address _receiver, uint256 toChainId, address tokenAddress, uint _amount) external payable nonReentrant override {
-		if(tokenAddress == NATIVE_TOKEN_ADDRESS) {
+	function transferNative(uint amount, address receiver, uint256 toChainId, string calldata tag) external payable nonReentrant {
+			require(msg.value == amount, "Wagpay: Please send amount greater than 0");
 			require(msg.value != 0, "WagPay: Please send amount greater than 0");
-			require(msg.value == _amount, "WagPay: Please send amount same to msg.value");
-			hyphenLiquidityPool.depositNative{value: _amount}(_receiver, toChainId);
-		} else {
-			require(msg.value == 0, "WagPay: msg.value should be zero");
-			require(_amount > 0, "WagPay: Please send amount greater than 0");
+			hyphenLiquidityPool.depositNative{value: amount}(receiver, toChainId, tag);
 
-			IERC20(tokenAddress).safeTransferFrom(_from, address(this), _amount);
-			IERC20(tokenAddress).safeIncreaseAllowance(address(hyphenLiquidityPool), _amount);
-
-			hyphenLiquidityPool.depositErc20(tokenAddress, _receiver, _amount, toChainId);
-		}
-		emit FundsTransferred(_receiver, toChainId, _amount, tokenAddress);
+			emit NativeFundsTransferred(receiver, toChainId);
 	}
+
+
+	function transferERC20(uint256 toChainId,
+        address tokenAddress,
+        address receiver,
+        uint256 amount,
+        string calldata tag ) external {
+
+			require(amount > 0, "WagPay: Please send amount greater than 0");
+
+			IERC20(tokenAddress).safeTransferFrom(msg.sender, address(this), amount);
+			IERC20(tokenAddress).safeIncreaseAllowance(address(hyphenLiquidityPool), amount);
+
+			hyphenLiquidityPool.depositErc20(toChainId, tokenAddress, receiver, amount, tag);
+		
+			emit ERC20FundsTransferred(receiver, toChainId, amount, tokenAddress);
+		}
 }
