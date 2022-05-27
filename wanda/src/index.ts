a6d732bbf3b85ffeb354148b10450d0ef5058a67
Satyam Kulkarni <punekar.satyam@gmail.com>
2022-05-27 23:47:24 +0530

    feat: made sdk more developer friendly by changing address to enums

--- wanda/src/index.ts
+++ wanda/src/index.ts
@@ -1,11 +1,9 @@
-import { Chain } from "./types/chain/chain.type";
-import { Coin } from "./types/coin/coin.type";
 import { _getRoutes } from "./services";
 import { ExecuteRouteData, RouteData, Routes, Token } from "./types";
 import { ethers } from "ethers";
 import { ApproveERC20, _checkApprove, _approve } from "./services/contract/evm/ERC20";
+import { ChainId } from "./types/chain/chain.enum";
 import { CoinKey } from "./types/coin/coin.enum";
-import axios from "axios";
 
 class WagPay {
 	
@@ -47,8 +45,18 @@ class WagPay {
 	executeRoute = async (route: ExecuteRouteData, signer: ethers.Signer): Promise<boolean | Error> => {
 		return new Promise(async (resolve, reject) => {
 			try {
-				const abi = [{"inputs":[{"components":[{"internalType":"address","name":"receiver","type":"address"},{"internalType":"address","name":"bridge","type":"address"},{"internalType":"uint256","name":"toChain","type":"uint256"},{"internalType":"address","name":"fromToken","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"},{"internalType":"bool","name":"dexRequired","type":"bool"},{"components":[{"internalType":"address","name":"dex","type":"address"},{"internalType":"uint256","name":"amountIn","type":"uint256"},{"internalType":"uint256","name":"fees","type":"uint256"},{"internalType":"uint256","name":"chainId","type":"uint256"},{"internalType":"address","name":"fromToken","type":"address"},{"internalType":"address","name":"toToken","type":"address"}],"internalType":"struct WagpayBridge.DexData","name":"dex","type":"tuple"}],"internalType":"struct WagpayBridge.RouteData","name":"_route","type":"tuple"}],"name":"transfer","outputs":[],"stateMutability":"payable","type":"function"}]
 				const address = await signer.getAddress()
+				
+				// @note - get erc20 approval
+				if(route.route.fromToken.address !== this.NATIVE_ADDRESS) {
+					const needed = await this.erc20ApproveNeeded(route.route.fromToken, address, route.route.amount.toString(), signer)
+
+					if(needed) {
+						await this.erc20Approve(route.route.fromToken, address, route.route.amount.toString(), signer)
+					}
+				}
+
+				const abi = [{"inputs":[{"components":[{"internalType":"address","name":"receiver","type":"address"},{"internalType":"address","name":"bridge","type":"address"},{"internalType":"uint256","name":"toChain","type":"uint256"},{"internalType":"address","name":"fromToken","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"},{"internalType":"bool","name":"dexRequired","type":"bool"},{"components":[{"internalType":"address","name":"dex","type":"address"},{"internalType":"uint256","name":"amountIn","type":"uint256"},{"internalType":"uint256","name":"fees","type":"uint256"},{"internalType":"uint256","name":"chainId","type":"uint256"},{"internalType":"address","name":"fromToken","type":"address"},{"internalType":"address","name":"toToken","type":"address"}],"internalType":"struct WagpayBridge.DexData","name":"dex","type":"tuple"}],"internalType":"struct WagpayBridge.RouteData","name":"_route","type":"tuple"}],"name":"transfer","outputs":[],"stateMutability":"payable","type":"function"}]
 				const contract = new ethers.Contract('0xc39863a88857fffc88695f2426Dd2157685e8F16', abi, signer)
 		
 				const routeData = {
@@ -98,19 +106,19 @@ class WagPay {
 
 }
 
-export default WagPay
+// export default WagPay
 
 // (async () => {
 // 	const wag = new WagPay()
 
 // 	const route = await wag.getRoutes({
-// 		fromChainId: 1,
-// 		toChainId: 137,
-// 		fromTokenAddress: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
-// 		toTokenAddress: '0xc2132D05D31c914a87C6611C10748AEb04B58e8F',
+// 		fromChain: ChainId.ETH,
+// 		toChain: ChainId.POL,
+// 		fromToken: CoinKey.ETH,
+// 		toToken: CoinKey.MATIC,
 // 		amount: '1000000000000000000'
 // 	})
-
+// 	console.log(route)
 // 	const token: Token = {
 // 		address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
 // 		chainId: 1,
@@ -118,9 +126,9 @@ export default WagPay
 // 		decimals: 6
 // 	}
 // 	console.log(route)
-// 	const provider = new ethers.providers.JsonRpcProvider('https://eth-mainnet.alchemyapi.io/v2/y141okG6TC3PecBM1mL0BfST9f4WQmLx')
-// 	let signer = ethers.Wallet.createRandom()
-// 	signer = signer.connect(provider)
+// 	// const provider = new ethers.providers.JsonRpcProvider('https://eth-mainnet.alchemyapi.io/v2/y141okG6TC3PecBM1mL0BfST9f4WQmLx')
+// 	// let signer = ethers.Wallet.createRandom()
+// 	// signer = signer.connect(provider)
 	
-// 	wag.executeRoute(route[0], signer)
+// 	// wag.executeRoute(route[0], signer)
 // })()
--- wanda/src/services/api/routes.ts
+++ wanda/src/services/api/routes.ts
@@ -1,14 +1,26 @@
 import { RouteData, Routes } from "../../types"
 import axios from "axios"
 import { CoinKey } from "../../types/coin/coin.enum"
-import { ChainId } from "../../types/chain/chain.enum"
+import tokens from "../../types/coin"
 
 
 export const _getRoutes = async (route: RouteData): Promise<Routes[]> => {
 	return new Promise(async (resolve, reject) => {
 		try {
+			console.log(route.fromChain, route.fromToken)
+			console.log(tokens[route.fromChain][route.fromToken])
+			let requestData = {
+				fromChainId: route.fromChain,
+				toChainId: route.toChain,
+				fromTokenAddress: tokens[route.fromChain][route.fromToken].address,
+				toTokenAddress: tokens[route.toChain][route.toToken].address,
+				amount: route.amount,
+				bridge: route.bridge,
+				dex: route.dex,
+			}
+
 			const response = await axios.get("http://localhost:5000/api/bridge/best-route", {
-				params: route
+				params: requestData
 			})
 
 			const routes: Routes[] = response.data
