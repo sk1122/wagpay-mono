8d4e98f84552ddad30a6235fb050b248cb0201a1
Shivam Alekar <shivam23@Shivams-MacBook-Air.local>
2022-05-30 01:04:00 +0530

    some bridge changes

--- captain/contracts/WagPayBridge.sol
+++ captain/contracts/WagPayBridge.sol
@@ -3,11 +3,17 @@ pragma solidity ^0.8.13;
 pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./interface/IBridge.sol";
 import "./interface/IDex.sol";
 
-contract WagPayBridge {
+contract WagPayBridge is Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _bridgeIds;
 	address private constant NATIVE_TOKEN_ADDRESS = address(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE);
+
+    mapping(uint => address) bridges;
 
     struct DexData {
         address dex;
@@ -36,7 +42,6 @@ contract WagPayBridge {
         IBridge bridge = IBridge(route.bridge);
 
         if(route.dexRequired) {
-            
             // Dex
             if(route.dex.fromToken == NATIVE_TOKEN_ADDRESS) {
                 idex.swapNative{value: route.amount}(route.dex.toToken, route.dex.extraData);
@@ -47,19 +52,31 @@ contract WagPayBridge {
 
             // Bridge
             if(route.fromToken == NATIVE_TOKEN_ADDRESS) {
-                bridge.transferNative{value: route.dex.amountIn}(route.dex.amountIn, route.receiver, route.toChain, "WagPay");
+                bridge.transferNative{value: route.dex.amountIn}(route.dex.amountIn, route.receiver, route.toChain, route.extraData);
             } else {
                 IERC20(route.fromToken).approve(route.bridge, route.dex.amountIn);
-                bridge.transferERC20(route.toChain, route.fromToken, route.receiver, route.dex.amountIn, "WagPay");
+                bridge.transferERC20(route.toChain, route.fromToken, route.receiver, route.dex.amountIn, route.extraData);
             }
+
         } else {
             // Bridge
             if(route.fromToken == NATIVE_TOKEN_ADDRESS) {
-                bridge.transferNative{value: route.amount}(route.amount, route.receiver, route.toChain, "WagPay");
+                bridge.transferNative{value: route.amount}(route.amount, route.receiver, route.toChain, route.extraData);
             } else {
                 IERC20(route.fromToken).approve(route.bridge, route.amount);
-                bridge.transferERC20(route.toChain, route.fromToken, route.receiver, route.amount, "WagPay");
+                bridge.transferERC20(route.toChain, route.fromToken, route.receiver, route.amount, route.extraData);
             }
         }
+    }
+
+    function addBridge(address newBridge) external onlyOwner returns (uint) {
+        _bridgeIds.increment();
+        uint bridgeId = _bridgeIds.current();
+        bridges[bridgeId] = newBridge;
+        return bridgeId;
+    }
+
+    function getBridge(uint _bridgeId) external view returns (address) {
+        return bridges[_bridgeId];
     }
 }
--- captain/contracts/interface/IDex.sol
+++ captain/contracts/interface/IDex.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.8.13;
 interface IDex {
 
     event NativeFundsSwapped(address tokenOut, uint amountIn, uint amountOut);
-	event ERC20FundsSwapped(uint amountIn, address tokenIn, address tokenOut, uint amountOut);
+    
+    event ERC20FundsSwapped(uint amountIn, address tokenIn, address tokenOut, uint amountOut);
 
     function swapNative(address _tokenOut,
         bytes memory extraData) external payable returns (uint amountOut);
