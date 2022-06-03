09387550ffb95c2569c9f353711c394146cc8407
= <=>
2022-06-03 18:03:10 +0530

    Developer page support.tsx componet added

--- thor/src/pages/DevPage.tsx
+++ thor/src/pages/DevPage.tsx
@@ -2,6 +2,7 @@ import CTA from '@/components/DeveloperPage/CTA';
 import Hero from '@/components/DeveloperPage/HeroSec';
 import Solustions from '@/components/DeveloperPage/Solutions';
 import Footer from '@/components/Footer';
+import Support from '@/components/landing/support';
 import Navbar from '@/components/Navbar';
 
 const DevPage = () => {
@@ -12,6 +13,10 @@ const DevPage = () => {
         <Hero />
         <CTA />
         <Solustions />
+        <div className="mx-auto w-full text-white lg:w-[80%]">
+          <Support />
+        </div>
+
         <Footer />
       </div>
     </>
