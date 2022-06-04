ad2d8da89cd790be858ca2bf549379486c3869a2
Shivam Alekar <shivam23@Shivams-MacBook-Air.local>
2022-06-04 17:34:30 +0530

    Hop changes

--- captain/contracts/bridges/HopBridge/HopProvider.sol
+++ captain/contracts/bridges/HopBridge/HopProvider.sol
@@ -10,22 +10,18 @@ import "../../interface/IBridge.sol";
 
 contract HopProvider is IBridge, Ownable, ReentrancyGuard {
 	using SafeERC20 for IERC20;
-	L2AMMSwapHop private hopBridge;
 
 	address private constant NATIVE_TOKEN_ADDRESS = address(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE);
-
-	constructor(address _hopBridge) {
-		hopBridge = L2AMMSwapHop(_hopBridge);
-	}
 	
 	function transferNative(uint amount, 
         address receiver, 
         uint64 toChainId, 
-        bytes memory //extraData
+        bytes memory extraData
 		) external payable nonReentrant {
 			require(msg.value != 0, "WagPay: Please send amount greater than 0");
 			require(msg.value == amount, "WagPay: Please send amount same to msg.value");
-			hopBridge.swapAndSend{value: amount}(toChainId, receiver, amount, 28578266354135306662, 0, 0, 0, 0);
+			(address bridgeAddress) = abi.decode(extraData, (address));
+			IHop(bridgeAddress).swapAndSend{value: amount}(toChainId, receiver, amount, 28578266354135306662, 0, 0, 0, 0);
 			emit NativeFundsTransferred(receiver, toChainId, amount);
 	}
 
@@ -33,15 +29,15 @@ contract HopProvider is IBridge, Ownable, ReentrancyGuard {
         address tokenAddress,
         address receiver,
         uint256 amount,
-        bytes memory //extraData
+        bytes memory extraData
 		) external nonReentrant {
 
 			require(amount > 0, "WagPay: Please send amount greater than 0");
-
+			(address bridgeAddress) = abi.decode(extraData, (address));
 			IERC20(tokenAddress).safeTransferFrom(msg.sender, address(this), amount);
-			IERC20(tokenAddress).safeIncreaseAllowance(address(hopBridge), amount);
+			IERC20(tokenAddress).safeIncreaseAllowance(address(bridgeAddress), amount);
 
-			hopBridge.swapAndSend(toChainId, receiver, amount, 28578266354135306662, 0, 0, 0, 0);
+			IHop(bridgeAddress).swapAndSend(toChainId, receiver, amount, 28578266354135306662, 0, 0, 0, 0);
 
 			emit ERC20FundsTransferred(receiver, toChainId, amount, tokenAddress);
 	}
