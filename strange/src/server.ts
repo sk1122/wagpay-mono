75a048a13d39f8ab0a9db2d14582a8e79e2f8274
Satyam Kulkarni <punekar.satyam@gmail.com>
2022-06-19 16:49:25 +0530

    strange: added wagpay.xyz to cors whitelist

--- strange/src/server.ts
+++ strange/src/server.ts
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(cors({
-    origin: ['http://localhost:3000']
+    origin: ['http://localhost:3000', "https://wagpay.xyz"]
 }))
 
 // Show routes called in console during development
